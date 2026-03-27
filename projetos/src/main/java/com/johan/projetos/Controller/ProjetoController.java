ba0b0e220645eb6d2bc11d915ca21e86e8d9f28c
Johan Gabriel <johan.universidade@gmail.com>
2026-03-26 21:32:32 -0300

    Rest realizado

--- projetos/src/main/java/com/johan/projetos/Controller/ProjetoController.java
+++ projetos/src/main/java/com/johan/projetos/Controller/ProjetoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/projetos")
+@RequestMapping(path = "/projetos")
 public class ProjetoController {
 
     @Autowired
